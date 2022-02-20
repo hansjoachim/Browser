45235add19d8852a3ad78534a32b18d4eec43fef
Hans Joachim Desserud <github@desserud.org>
2022-02-20 10:23:07 +0100

    Add HTTPClient to fetch a webpage and, for now, print the raw content

--- src/main/kotlin/org/example/Hello.kt
+++ src/main/kotlin/org/example/Hello.kt
@@ -1,6 +1,18 @@
 package org.example
 
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse.BodyHandlers
+
 fun main(args: Array<String>) {
+    val request = HttpRequest.newBuilder(URI(args[0]))
+        .GET()
+        .build()
+    val client = HttpClient.newBuilder().build()
+
+    val result = client.send(request, BodyHandlers.ofString())
     println("Hello, World")
+    println(result.body())
 }
 
