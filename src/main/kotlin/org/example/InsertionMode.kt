2318122be04c95a19d6e5b6836fd4e2a5463af41
Hans Joachim Desserud <github@desserud.org>
2022-02-20 19:05:22 +0100

    Enumerate InsertionModes in preparation for parsing our tokens

---
+++ src/main/kotlin/org/example/InsertionMode.kt
@@ -0,0 +1,27 @@
+package org.example
+
+enum class InsertionMode {
+    initial,
+    beforeHtml,
+    beforeHead,
+    inHead,
+    inHeadNoscript,
+    afterHead,
+    inBody,
+    text,
+    inTable,
+    inTableText,
+    inCaption,
+    inColumnGroup,
+    inTableBody,
+    inRow,
+    inCell,
+    inSelect,
+    inSelectInTable,
+    inTemplate,
+    afterBody,
+    inFrameset,
+    afterFrameset,
+    afterAfterBody,
+    afterAfterFrameset
+}
